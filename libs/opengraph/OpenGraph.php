4929686ce555876b5fa32900b0d158d52bd678f0
Sebastian Buckpesch <s.buckpesch@iconsultants.eu>
2013-02-05 10:50:33 +0100

    debuggin rss reader

--- libs/opengraph/OpenGraph.php
+++ libs/opengraph/OpenGraph.php
@@ -182,3 +182,67 @@ class OpenGraph implements Iterator
 	public function next() { next($this->_values); ++$this->_position; }
 	public function valid() { return $this->_position < sizeof($this->_values); }
 }
+
+<?php
+   /**
+    * regexp helper ,for easy use regexp
+    */
+   class Frd_Regexp
+   {
+       public static function replace($string,$pattern,$replace,$limit=-1)
+       {
+           $pattern="/".trim($pattern,"/")."/";
+
+           $string=preg_replace($pattern,$replace,$string,$limit);
+
+           return $string;
+       }
+
+       public static function search($string,$pattern)
+       {
+           $pattern="/".trim($pattern,"/")."/";
+           $match='';
+
+
+           if(preg_match($pattern,$string,$match) === false)
+           {
+               throw new Exception("preg_match falied ");
+           }
+
+           return $match;
+       }
+
+       public static function searchAll($string,$pattern)
+       {
+           $match=array();
+           $pattern="/".trim($pattern,"/")."/";
+
+           if(preg_match_all($pattern,$string,$match) === false)
+           {
+               throw new Exception("preg_match falied ");
+           }
+
+           return $match;
+       }
+
+       public static function isMatch($string,$pattern)
+       {
+           $match='';
+           //$pattern="/^$pattern$/";
+           $pattern="/$pattern/";
+
+           if(preg_match($pattern,$string,$match) === false)
+           {
+               throw new Exception("preg_match falied ");
+           }
+
+           if($match == false)
+           {
+               return false;
+           }
+           else
+           {
+               return true;
+           }
+       }
+   }
